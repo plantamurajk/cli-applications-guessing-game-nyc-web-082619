02c270009506db7e0af4bada10df108898a661bf
John Plantamura <plantamurajk@gmail.com>
2019-08-26 21:37:38 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   
   guess = gets.chomp
   
-  if (guess == num) then
+  if (guess == num)
     puts "You guessed the correct number!"
   else if (guess == 'exit')
     puts "Goodbye!"
