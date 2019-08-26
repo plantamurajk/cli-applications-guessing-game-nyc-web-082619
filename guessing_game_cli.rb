3af2344aa065f53c6e85986c6a62001a64c1200a
John Plantamura <plantamurajk@gmail.com>
2019-08-26 21:23:21 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,4 +3,13 @@
 def run_guessing_game
   num = rand(1..6)
   
-  gets guess
+  guess = gets
+  
+  if (guess == num) then
+    puts "You guessed the correct number!"
+  else if (guess == 'exit')
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{num}."
+  end
+end
